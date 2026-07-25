ca90b57d2aa2c57fe6e347aa5f317ab05028df1b
1Zamuken1 <juanbarrios072@gmail.com>
2026-07-25 11:32:12 -0500

    fix: use map instead of tap in getStaticSkillsFlat() for correct type transformation

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, inject, isDevMode } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, of, tap, map } from 'rxjs';
 import { Project } from '../../shared/models/project.model';
 import { Experience } from '../../shared/models/experience.model';
 import { AdminSkill, SkillCategory, Skill } from '../../shared/models/skill.model';
@@ -31,7 +31,7 @@ export class DataService {
 
   getStaticSkillsFlat(): Observable<AdminSkill[]> {
     return this.getStaticSkills().pipe(
-      tap((categories) => {
+      map((categories) => {
         const flat: AdminSkill[] = [];
         let order = 1;
         categories.forEach((cat) => {
